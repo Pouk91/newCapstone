728dbe2d1190a761fb95b23cf76d906701937434
Pouk91 <pinaro.ouk@gmail.com>
2017-06-16 02:19:16 -0400

    Removed / from production URL in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://stormy-crag-69533.herokuapp.com/',
+    production: 'https://stormy-crag-69533.herokuapp.com',
     development: 'http://localhost:4741'
   }
 }
